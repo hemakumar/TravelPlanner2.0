cec1ada97c67a6a5fa274367c5d89b2fd0aa9b86
Hema kumar <hemasid@gmail.com>
2012-03-13 02:30:23 -0700

    Source Files-2

--- TravelPlanner 2.1/src/com/TravelPlanner/NewPlan.java
+++ TravelPlanner 2.1/src/com/TravelPlanner/NewPlan.java
@@ -1,3 +1,17 @@
+//Open-Android-TravelPlanner Copyright © 2012
+//@author "Hema Kumar"
+////** This file is part of TravelPlanner2.1 .This is free software: you can redistribute it
+//* and/or modify it under the terms of the GNU General Public License as published by the
+//* Free Software Foundation, either version 3 of the License, or any later version.
+//* Travel Planner is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//* without even the implied warranty ofMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//* See theGNU General Public License for more details.
+//*
+//* You should have received a copy of the GNU General Public License along with TravelPlanner2.1.
+//* If not, see <http://www.gnu.org/licenses/>.
+//* For feedback please mail at email id hemasid@gmail.com 
+
+
 package com.TravelPlanner;
 
 import java.util.Calendar;
